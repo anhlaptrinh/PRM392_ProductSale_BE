75642b501bea456ea5da80e22e717309cf0054a0
chinh3571113 <chinh3571113@gmail.com>
2025-05-31 15:44:58 +0700

    inital commit

---
+++ src/main/java/com/prm/productsale/repository/UserRepo.java
@@ -0,0 +1,14 @@
+package com.prm.productsale.repository;
+
+import com.prm.productsale.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepo extends JpaRepository<UserEntity,Integer> {
+  Optional<UserEntity> findByEmail(String email);
+  Optional<UserEntity> findById(int id);
+}
---
+++ src/main/java/com/prm/productsale/services/UserServices.java
@@ -0,0 +1,4 @@
+package com.prm.productsale.services;
+
+public interface UserServices {
+}
