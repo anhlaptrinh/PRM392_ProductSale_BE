76ec80752e4744ed73661a9eefdca2aa83754e30
Bikann <vipkene15@gmail.com>
2025-07-18 23:57:28 +0700

    Fix Cart API

--- src/main/java/com/prm/productsale/repository/CartRepo.java
+++ src/main/java/com/prm/productsale/repository/CartRepo.java
@@ -4,10 +4,11 @@ import com.prm.productsale.entity.CartEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CartRepo extends JpaRepository<CartEntity,Integer> {
     Optional<CartEntity> findByUserId(int userId);
-    Optional<CartEntity> findFirstByUser_IdAndStatusOrderByIdDesc(int userId, String status);
+    List<CartEntity> findByUser_IdAndStatus(int userId, String status);
 }
